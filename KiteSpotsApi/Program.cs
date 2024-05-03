ee7aa2c35a572e97664bf3b35ce62cef45970d1a
SebMoya <sebastianmoya@live.se>
2024-05-03 11:48:16 +0200

    update

--- KiteSpotsApi/Program.cs
+++ KiteSpotsApi/Program.cs
@@ -1,4 +1,16 @@
+using Common.DTO;
+using Common.Interface;
+using DataAccess;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("KiteSpotsLocal");
+
+builder.Services.AddScoped<ISpotService<Spot>, SpotsRepository>();
+builder.Services.AddDbContext<KiteSpotsDbContext>(options =>
+       options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
