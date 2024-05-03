aff29d1418a17ea0f23e8dbae6c36f5b57e26ab5
SebMoya <sebastianmoya@live.se>
2024-05-03 08:22:22 +0200

    Add project files.

---
+++ DataAccess/Entities/Spot.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities;
+
+public class Spot
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public List<string> Properties { get; set; }
+
+    public string Image { get; set; }
+
+    public List<string> WindDirectionGood { get; set; }
+
+    public List<string> WindDirectionOk { get; set; }
+
+    public List<string> WindDirectionBad { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public string Area { get; set; }
+
+}
